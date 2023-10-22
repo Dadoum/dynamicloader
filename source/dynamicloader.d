3da83ef0f6d1d39b1cb3d7c18603cb4444fedb99
Dadoum <24679280+Dadoum@users.noreply.github.com>
2023-10-22 17:49:01 +0200

    Load all the symbols at once, and fix a weird archlinux-only heisenbug

--- source/dynamicloader.d
+++ source/dynamicloader.d
@@ -31,6 +31,7 @@ public struct LibImport {
                     return;
                 }
             }
+
             throw new LibraryLoadingException(format!"Cannot load any of the following libraries: %s"(library.libraries));
         }
 
@@ -43,63 +44,91 @@ public struct LibImport {
         //     }
         // }
     }
+
+    template functionStore(alias func) {
+        __gshared void* functionStore;
+    }
 }
 
 struct AlternateName {
     string name;
 }
 
-mixin template bindFunction(alias symbol) {
+mixin template bindFunction(alias symbol, alias functionLoader) {
+    alias lib = getUDAs!(symbol, LibImport)[0];
+
+    alias FunctionType = typeof(&symbol);
+    enum mangledName = symbol.mangleof;
+
+    alias loadedFunction = LibImport.functionStore!symbol;
+
+    pragma(mangle, mangledName)
+    extern (C) auto impl(Parameters!symbol params) @(__traits(getAttributes, symbol)) {
+        if (!LibImport.loaded!lib) {
+            LibImport.load!lib();
+            functionLoader();
+        }
+
+        return (cast(FunctionType) loadedFunction)(params);
+    }
+}
+
+template loadFunction(alias symbol) {
+    alias lib = getUDAs!(symbol, LibImport)[0];
+    import std.traits: getUDAs, getSymbolsByUDA, ReturnType, Parameters;
     import core.sys.posix.dlfcn;
     import core.sys.windows.windows;
 
     import std.algorithm.iteration;
     import std.array;
     import std.format;
-
-    alias lib = getUDAs!(symbol, LibImport)[0];
     enum alternateNames = (cast(AlternateName[]) [getUDAs!(symbol, AlternateName)]).map!((alternateName) => alternateName.name).array();
 
     alias FunctionType = typeof(&symbol);
     enum mangledName = symbol.mangleof;
 
-    __gshared void* loadedFunction;
+    alias loadedFunction = LibImport.functionStore!symbol;
 
-    void ensureFunctionIsLoaded() {
-        if (!LibImport.loaded!lib) {
-            LibImport.load!lib();
-        }
+    void loadFunction() {
+        alias loadedFunction = LibImport.functionStore!symbol;
 
-        if (!loadedFunction) {
-            auto library = LibImport.libraryHandle!lib;
-            assert(library != null);
+        auto library = LibImport.libraryHandle!lib;
+        assert(library != null);
 
-            static foreach (name; alternateNames ~ mangledName) {
-                version (Windows) {
-                    loadedFunction = GetProcAddress(library, name);
-                } else {
-                    loadedFunction = dlsym(library, name);
-                }
-                if (loadedFunction) {
-                    return;
-                }
+        static foreach (name; alternateNames ~ mangledName) {
+            version (Windows) {
+                loadedFunction = GetProcAddress(library, name);
+            } else {
+                loadedFunction = dlsym(library, name);
             }
-            throw new LibraryLoadingException(format!"Cannot load %s, tried to load %s"(__traits(identifier, symbol), alternateNames ~ mangledName));
+            if (loadedFunction) {
+                return;
+            }
         }
-    }
-
-    pragma(mangle, mangledName)
-    extern (C) ReturnType!symbol impl(Parameters!symbol params) @(__traits(getAttributes, symbol)) {
-        ensureFunctionIsLoaded();
-        return (cast(FunctionType) loadedFunction)(params);
+        throw new LibraryLoadingException(format!"Cannot load %s, tried to load %s"(__traits(identifier, symbol), alternateNames ~ mangledName));
     }
 }
 
 mixin template makeBindings() {
     import std.traits: getUDAs, getSymbolsByUDA, ReturnType, Parameters;
-    static foreach (symbol; getSymbolsByUDA!(__traits(parent, {}), LibImport)) {
+    import core.sys.posix.dlfcn;
+    import core.sys.windows.windows;
+
+    import std.algorithm.iteration;
+    import std.array;
+    import std.format;
+    alias module_ = __traits(parent, {});
+    static foreach (symbol; getSymbolsByUDA!(module_, LibImport)) {
         static if (is(typeof(symbol) == function)) {
-            mixin bindFunction!(symbol); // static foreach doesn't introduce a scope but mixin does so we use a mixin.
+            mixin bindFunction!(symbol, loadFunctions); // static foreach doesn't introduce a scope but mixin does so we use a mixin.
+        }
+    }
+
+    void loadFunctions() {
+        static foreach (symbol; getSymbolsByUDA!(module_, LibImport)) {
+            static if (is(typeof(symbol) == function)) {
+                loadFunction!symbol();
+            }
         }
     }
 }
